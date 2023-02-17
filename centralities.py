1926784bb15fc6dd3e2718d01a5f8f7f0de32942
andrei <andrei.ionita@rwth-aachen.de>
2023-02-17 19:37:50 +0100

    added horizonatal bar charts for In and out Degree distribution

--- centralities.py
+++ centralities.py
@@ -44,17 +44,24 @@ address_map['0xe56969d8a0b333b286f3023efbcd3c112216c4bf']='glenfiddichwhisky.eth
 address_map['0xd4734ce5102715924ffd44a41d7bb785ca2e6743']='shwaz.eth'
 address_map['0xea94fa609015830e368a685a464a82c2ac20cacd']='onxy.eth'
 address_map['0xace69a426da0857043938bc38701cb6c16feea04']='ardbegwhisky.eth'
+address_map['0x86a41524cb61edd8b115a72ad9735f8068996688']='whalen.eth'
 
 DG = nx.from_pandas_edgelist(
     df=all_tx_labels,
     source='From_node_label',
     target='To_node_label',
     edge_attr='Token_ID',
+    #create_using=nx.MultiDiGraph()
     create_using=nx.DiGraph()
 )
 
 #centralities = dict(sorted(nx.in_degree_centrality(DG).items(), key=lambda x:x[1], reverse=True))
-centralities = dict(sorted(nx.out_degree_centrality(DG).items(), key=lambda x:x[1], reverse=True))
+#centralities = dict(sorted(nx.out_degree_centrality(DG).items(), key=lambda x:x[1], reverse=True))
+#centralities = dict(sorted(nx.betweenness_centrality(DG).items(), key=lambda x:x[1], reverse=True))
+#centralities = dict(sorted(nx.closeness_centrality(DG).items(), key=lambda x:x[1], reverse=True))
+#centralities = dict(sorted(nx.clustering(DG).items(), key=lambda x:x[1], reverse=True))
+#centralities = dict(sorted(DG.in_degree(), key=lambda x:x[1], reverse=True))
+centralities = dict(sorted(DG.out_degree(), key=lambda x:x[1], reverse=True))
 centralities_labels = [label_map[item] for item in list(centralities.keys())]
 print(centralities_labels[:10])
 centralities_names = [address_map[item] if item in address_map else item for item in centralities_labels]
@@ -62,6 +69,12 @@ centralities_names = [address_map[item] if item in address_map else item for ite
 fig, ax = plt.subplots(figsize=[5,4])
 ax.barh(list(reversed(centralities_names[:10])), list(reversed(list(centralities.values())[:10])))
 #ax.set_title('In-Degree Centrality')
-ax.set_title('Out-Degree Centrality')
+#ax.set_title('Out-Degree Centrality')
+#ax.set_title('Betweenness Centrality')
+#ax.set_title('Closeness Centrality')
 plt.subplots_adjust(left=0.23)
 plt.show()
+
+#print("Network diameter: " + str(nx.diameter(DG)))
+print("Average shortest path length: " + str(nx.average_shortest_path_length(DG)))
+print(max(nx.strongly_connected_components(DG), key=len))
